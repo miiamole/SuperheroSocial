e3fa2e2914bf96d2b9eb46633534b100f8a8e130
miiamole <miia_pelaez@hotmail.com>
2024-02-06 15:23:23 +0100

    changes

--- SuperheroSocial/Pages/Change.cshtml.cs
+++ SuperheroSocial/Pages/Change.cshtml.cs
@@ -6,18 +6,30 @@ namespace SuperheroSocial.Pages
     public class ChangeModel : PageModel
     {
         [BindProperty]
-        public List<SuperheroModel> heroes { get; set; }
-        public void OnGet()
+        //public List<SuperheroModel> heroes { get; set; }
+        public int Id { get; set; }
+        [BindProperty]
+        public SuperheroModel Hero { get; set; }
+        public IActionResult OnGet(int id)
         {
-
+            Id = id;
+            Hero = SuperheroManager.heroes.FirstOrDefault(s => s.Id == id);
             //Hej
+            if (Hero == null)
+            {
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                return Page();
+            }
 
         }
 
 
         public IActionResult OnPut(int id, SuperheroModel updatedSuperhero)
         {
-            var item = heroes.FirstOrDefault(s => s.Id == id);
+            var item = SuperheroManager.heroes.FirstOrDefault(s => s.Id == id);
             if (item == null)
             {
                 return Page();
@@ -28,6 +40,8 @@ namespace SuperheroSocial.Pages
                 item.Name = updatedSuperhero.Name;
                 item.SecretIdentity = updatedSuperhero.SecretIdentity;
                 item.SuperPower = updatedSuperhero.SuperPower;
+
+                SuperheroManager.heroes.Add(updatedSuperhero);
 
                 return RedirectToPage("/index");
             }

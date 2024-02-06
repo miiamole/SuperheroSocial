8070e27c99abae783fdde5d022492fc9cc028c41
EmmaDQ <sydemma@hotmail.com>
2024-02-06 13:09:39 +0100

    add changes superpowermodel

--- SuperheroSocial/Models/SuperpowerModel.cs
+++ SuperheroSocial/Models/SuperpowerModel.cs
@@ -1,9 +1,7 @@
 ﻿namespace SuperheroSocial
 {
-	public class SuperpowerModel
-	{
-		public string? Superpower1 { get; set; }
-		public string? Superpower2 { get; set; }
-		public string? Superpower3 { get; set; }
-	}
+    public class SuperpowerModel
+    {
+        public string? Superpower { get; set; }
+    }
 }
--- SuperheroSocial/Pages/Change.cshtml.cs
+++ SuperheroSocial/Pages/Change.cshtml.cs
@@ -3,35 +3,35 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SuperheroSocial.Pages
 {
-	public class ChangeModel : PageModel
-	{
-		[BindProperty]
-		public List<SuperheroModel> heroes { get; set; }
-		public void OnGet()
-		{
+    public class ChangeModel : PageModel
+    {
+        [BindProperty]
+        public List<SuperheroModel> heroes { get; set; }
+        public void OnGet()
+        {
+
+            //Hej
+
+        }
 
 
+        public IActionResult OnPut(int id, SuperheroModel updatedSuperhero)
+        {
+            var item = heroes.FirstOrDefault(s => s.Id == id);
+            if (item == null)
+            {
+                return Page();
+            }
+            else
+            {
+                item.Id = updatedSuperhero.Id;
+                item.Name = updatedSuperhero.Name;
+                item.SecretIdentity = updatedSuperhero.SecretIdentity;
+                item.SuperPower = updatedSuperhero.SuperPower;
 
-		}
+                return RedirectToPage("/index");
+            }
 
-
-		public IActionResult OnPut(int id, SuperheroModel updatedSuperhero)
-		{
-			var item = heroes.FirstOrDefault(s => s.Id == id);
-			if (item == null)
-			{
-				return Page();
-			}
-			else
-			{
-				item.Id = updatedSuperhero.Id;
-				item.Name = updatedSuperhero.Name;
-				item.SecretIdentity = updatedSuperhero.SecretIdentity;
-				item.SuperPower = updatedSuperhero.SuperPower;
-
-				return RedirectToPage("/index");
-			}
-
-		}
-	}
+        }
+    }
 }

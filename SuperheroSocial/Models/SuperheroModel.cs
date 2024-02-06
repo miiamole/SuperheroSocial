93e02e72eb25cd4268fe22e8a949bd309dd01c7a
miiamole <miia_pelaez@hotmail.com>
2024-02-06 10:04:51 +0100

    model

--- SuperheroSocial/Models/SuperheroModel.cs
+++ SuperheroSocial/Models/SuperheroModel.cs
@@ -2,5 +2,17 @@
 {
 	public class SuperheroModel
 	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string? SecretIdentity { get; set; }
+
+		public List<SuperpowerModel> SuperPower { get; set; }
+
+		public string? Image { get; set; }
+
+
+
 	}
 }

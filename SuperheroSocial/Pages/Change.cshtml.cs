b348cbcee3ad9b00262da9a24f87eaa6fb1f57af
miiamole <miia_pelaez@hotmail.com>
2024-02-06 10:55:32 +0100

    changes

--- SuperheroSocial/Pages/Change.cshtml.cs
+++ SuperheroSocial/Pages/Change.cshtml.cs
@@ -3,10 +3,32 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SuperheroSocial.Pages
 {
-    public class ChangeModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class ChangeModel : PageModel
+	{
+		[BindProperty]
+		public SuperheroModel Superheroes { get; set; }
+		public void OnGet()
+		{
+
+		}
+
+		public IActionResult OnPut(int id, SuperheroModel updatedSuperhero)
+		{
+			var item = Superheroes.FirstOrDefault(s => s.Id == id);
+			if (item == null)
+			{
+				return Page();
+			}
+			else
+			{
+				item.Id = updatedSuperhero.Id;
+				item.Name = updatedSuperhero.Name;
+				item.SecretIdentity = updatedSuperhero.SecretIdentity;
+				item.SuperPower = updatedSuperhero.SuperPower;
+
+				return
+			}
+
+		}
+	}
 }
